0680f0af743ad010604bad04ae7d7db63e38ad15
Robby Kloss <robk113355@gmail.com>
2025-03-06 13:26:02 -0600

    changed retVal

--- maths.cpp
+++ maths.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
-void power(int base, int exponent)
+int power(int base, int exponent)
 {
     int retVal = base;
     for (int i = 0; i < exponent-1; i++)
     {
         retVal = retVal * base;
     }
-    cout << retVal;
+
+    return retVal;
 }
 
 int main()
 {
     cout << "GitHub Test\n";
-    power(2, 3);
+    cout << power(2, 3);
 }
 
