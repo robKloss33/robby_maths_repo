a1ef719b0ca5578ca1a15c6e505853850aeef7a2
Robby Kloss <robk113355@gmail.com>
2025-03-06 13:45:28 -0600

    added a nextPrime function

--- maths.cpp
+++ maths.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 using namespace std;
+int nextPrime(int start)
+{
+    bool isPrime = false;
+
+    start++;
+    while (!isPrime) {
+        for (int i = 2; i < start; i++)
+        {
+            if (start % i == 0)
+            {
+                break;
+            }
+            if (i == start - 1)
+            {
+                return start;
+            }
+        }
+
+        start++;
+    }
+    return start;
+}
 double power(double base, int exponent)
 {
     double retVal = base;
@@ -15,5 +37,6 @@ int main()
 {
     cout << "GitHub Test\n";
     power(2.0, 3);
+    cout << nextPrime(5) << endl;
 }
 
