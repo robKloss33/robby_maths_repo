8e08032e94c3d5a53f40b82e7811f76667c4bf6d
daniel-plutchak <dplutchak@carthage.edu>
2025-03-06 13:49:59 -0600

    added sqrt function

--- maths.cpp
+++ maths.cpp
@@ -11,9 +11,27 @@ double power(double base, int exponent)
     return retVal;
 }
 
+double sqrt(double num) {
+    if (num < 0) {
+        return -1;
+    }
+    if (num == 0 || num == 1) return num;
+
+    double guess = num / 2.0;
+    double prev_guess;
+
+    do {
+        prev_guess = guess;
+        guess = (guess + num / guess) / 2.0;
+    } while (guess != prev_guess);
+
+    return guess;
+}
+
 int main()
 {
     cout << "GitHub Test\n";
     power(2.0, 3);
+    cout << sqrt(51) << endl;
 }
 
