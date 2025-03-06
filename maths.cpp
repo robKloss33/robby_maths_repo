ef31594a310faa0f31c67c84ed784316dac045cb
Robby Kloss <robk113355@gmail.com>
2025-03-06 13:12:32 -0600

    added square function

--- maths.cpp
+++ maths.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+void square(int x)
+{
+    int retVal = x * x;
+    cout << retVal;
+}
+
 int main()
 {
     cout << "GitHub Test\n";
+    square(10);
 }
+
